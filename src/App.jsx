dcf81f8ec4c81ddfa43c5e834ac2538592048766
菅野琴実 <kannokotomi@kannokotojitsunoMacBook-Air.local>
2026-07-17 10:53:50 +0900

    Reactアプリを追加

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+function App() {
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        flexDirection: "column",
+        background: "#f5f7fb",
+        fontFamily: "sans-serif",
+      }}
+    >
+      <h1>🎉 初めてのReactアプリ</h1>
+
+      <h2>神山高専3年</h2>
+
+      <p>GitHub Actionsで公開しました！</p>
+    </div>
+  );
+}
+
+export default App;
